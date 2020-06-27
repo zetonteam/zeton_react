692b0839176b77f6ea21034d568388c9258f4f76
Paweł <pawel.nieczuja@onet.eu>
2020-06-27 11:16:58 +0200

    Add Material UI

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,22 +5,22 @@ import './Footer.css';
 
 
 class Footer extends Component {
-    render() {
-        return (
-            <HashRouter>
-                <div>
-                    <ul className="navigation">
-                        <li><NavLink to="/home">Home</NavLink></li>
-                        <li><NavLink to="/stuff">Nagrody</NavLink></li>
-                        <li><NavLink to="/contact">Konsekwencje</NavLink></li>
-                        <li><NavLink to="/contact">Ustawienia</NavLink></li>
-                    </ul>
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <ul className="navigation">
+            <li><NavLink to="/home">Home</NavLink></li>
+            <li><NavLink to="/stuff">Nagrody</NavLink></li>
+            <li><NavLink to="/contact">Konsekwencje</NavLink></li>
+            <li><NavLink to="/contact">Ustawienia</NavLink></li>
+          </ul>
 
-                </div>
-            </HashRouter>
+        </div>
+      </HashRouter>
 
-        )
-    }
+    )
+  }
 }
 
 export default Footer
