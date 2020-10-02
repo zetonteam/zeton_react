6b56b613278887b86f7419a88e6d3cfdef867054
maknetaRo <makneta@gmail.com>
2020-10-02 21:05:33 +0200

    Updates

---
+++ precache-manifest.a7800ecd45f28f53276c1b8035fb405e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "272e1cecec2c38818d0d094577fbd6eb",
+    "url": "/zeton_react/index.html"
+  },
+  {
+    "revision": "d610ccbc3797b9e7c729",
+    "url": "/zeton_react/static/css/main.7a14c0c0.chunk.css"
+  },
+  {
+    "revision": "53723757aabb96473a47",
+    "url": "/zeton_react/static/js/2.413eff38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zeton_react/static/js/2.413eff38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d610ccbc3797b9e7c729",
+    "url": "/zeton_react/static/js/main.6bf7b6b7.chunk.js"
+  },
+  {
+    "revision": "d86abfcd5fe664273213",
+    "url": "/zeton_react/static/js/runtime-main.8b1240ce.js"
+  },
+  {
+    "revision": "c10242129cb7b6b2c77ec7ac397c7a89",
+    "url": "/zeton_react/static/media/LOGO.c1024212.svg"
+  }
+]);
