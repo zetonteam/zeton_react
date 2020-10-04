63c713352f39b565a226208746bf722479871275
Stas Miotk <tgb.stas@hotmail.com>
2020-10-04 14:12:54 +0200

    add button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import logo from './images/icons/LOGO.svg';
 import './App.css';
 import Footer from './components/Footer/Footer'
 
+const Przycisk=()=>{
+
+return(
+<Button variant="contained" color="primary">
+  test
+</Button>
+);
+
+}
+
 function App() {
   return (
     <div className="App">
@@ -11,6 +21,7 @@ function App() {
         <p>
           Witaj w aplikacji "Żeton"!
         </p>
+        <Przycisk />
       </header>
       <Footer />
     </div>
