27339a6c5dfea7bc15ac5fe31fe653398565f63e
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2020-10-04 14:09:29 +0200

    Add login elems

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,26 @@
 import React from "react";
-
+import { FormControl, TextField } from '@material-ui/core';
 
 const Login = () => {
   return (
-    <h1>Login</h1>
+    <section className="login">
+      <FormControl>
+        <h1>Login</h1>
+        <div>
+          <TextField
+            id="outlined-basic"
+            label="Username"
+            variant="outlined" />
+          <TextField
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            variant="outlined"
+          />
+        </div>
+      </FormControl>
+    </section>
   )
 }
 
