c7cf03fd740f6f4ec4e6b86d489351e227126de9
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2020-10-04 13:16:56 +0200

    Add animation effect to welcome

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './images/icons/LOGO.svg';
 import './App.css';
 //import Footer from './components/Footer/Footer'
 
 function App() {
+  const [welcome, toggleWelcome] = useState(false)
+
+  useState(() => {
+    const timer = setTimeout(() => {
+      toggleWelcome(true)
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <aside className="welcome">
+      <aside className={welcome === false ? "welcome" : "welcome--off"}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Witaj w aplikacji "Żeton"!
         </p>
       </aside>
+      <Login />
       {/* <Footer /> */}
     </div>
   );
