7d4af29a15ef54c3111e0b921c1c686c03601f08
Mikołaj Lewandowski <mikolevy1@gmail.com>
2020-10-05 16:40:23 +0200

    - Add api client - Pull student name from API

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import logo                           from "../../images/icons/LOGO.svg";
 import HomeIcon                       from "../../components/UI/HomeIcon/HomeIcon";
 import BasicButton                    from "../../components/UI/Button/Button";
+import apiClient                      from "../../apiClient";
 
 const DashboardPage = () => {
   const [showSplashScreen, toggleWelcome] = useState(true);
   const [points, setPoints] = useState(50);
   const [exp, setExp] = useState(35);
-  const [childName, setChildName] = useState("Wojtek")
+  const [studentName, setStudentName] = useState("-")
+  const studentDataURL = "/api/users/students/1";
 
   useEffect(() => {
     if (showSplashScreen) {
@@ -17,6 +19,21 @@ const DashboardPage = () => {
       return () => clearTimeout(timer);
     }
   }, [showSplashScreen]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      const result = await apiClient(studentDataURL);
+
+      setStudentName(result.data.first_name);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [studentDataURL]);
 
   const addPoints = () => {
     setPoints(points + 1);
@@ -32,7 +49,7 @@ const DashboardPage = () => {
         </p>
       </aside>
 
-      {childName}
+      {studentName}
 
       <HomeIcon color="primary"/>
       <div>{points}</div>
