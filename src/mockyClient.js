ccb7010e0ee074342eb114c1da3364f725daee0a
Leszek Miotk <tgb.dc7@gmail.com>
2020-11-05 21:06:15 +0100

    Api mocky client (#9)
    * Add mockyClient
    
    * Clear points
    
    * Set mockyClient points
    
    * Update styles
    
    * Update styles
    
    Co-authored-by: Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>

---
+++ src/mockyClient.js
@@ -0,0 +1,11 @@
+export const mockyClient = (url) => {
+  return {
+    data: {
+      first_name: "Janusz",
+      last_name: "Nowak",
+      total_points: 150,
+      daily_points: 70,
+      exp_points: 56
+    }
+  }
+}
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
-import logo                           from "../../images/icons/LOGO.svg";
-import HomeIcon                       from "../../components/UI/HomeIcon/HomeIcon";
-import BasicButton                    from "../../components/UI/Button/Button";
-import apiClient                      from "../../apiClient";
+import logo from "../../images/icons/LOGO.svg";
+import HomeIcon from "../../components/UI/HomeIcon/HomeIcon";
+import BasicButton from "../../components/UI/Button/Button";
+import apiClient from "../../apiClient";
+import { Box, Container, Typography } from '@material-ui/core';
 
 const DashboardPage = () => {
   const [showSplashScreen, toggleWelcome] = useState(true);
-  const [points, setPoints] = useState(50);
-  const [exp, setExp] = useState(35);
+  const [points, setPoints] = useState(0);
+  const [dailyPoints, setDailyPoints] = useState(0);
+  const [exp, setExp] = useState(0);
   const [studentName, setStudentName] = useState("-")
   const studentDataURL = "/api/users/students/1";
 
@@ -29,6 +31,9 @@ const DashboardPage = () => {
       const result = await apiClient(studentDataURL);
 
       setStudentName(result.data.first_name);
+      setPoints(result.data.total_points);
+      setDailyPoints(result.data.daily_points);
+      setExp(result.data.exp_points);
       setIsLoading(false);
     };
 
@@ -37,38 +42,49 @@ const DashboardPage = () => {
 
   const addPoints = () => {
     setPoints(points + 1);
-    setExp(exp + 1);
+    setDailyPoints(dailyPoints + 1);
   };
 
   return (
     <>
       <aside className={showSplashScreen === true ? "welcome" : "welcome--off"}>
-        <img src={logo} className="App-logo" alt="logo"/>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Witaj w aplikacji "Żeton"!
         </p>
       </aside>
 
-      {studentName}
+      <Container>
+        <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" padding={1}>
+          <Typography variant="h5">{studentName}</Typography>
+        </Box>
 
-      <HomeIcon color="primary"/>
-      <div>{points}</div>
-      <br/>
+        <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" padding={1}>
+          <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" padding={1}>
+            <HomeIcon color="primary" />
+            <Typography variant="p">{points}</Typography>
+          </Box>
+          <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" padding={1}>
+            <HomeIcon color="secondary" />
+            <Typography variant="p">{exp}</Typography>
+          </Box>
 
-      <HomeIcon color="secondary"/>{exp}
+        </Box>
+      </Container>
 
-      <BasicButton
-        label="DODAJ PUNKT"
-        onClick={addPoints}
-      />
+      <Container>
+        <BasicButton
+          label="DODAJ PUNKT"
+          onClick={addPoints}
+        />
 
-      <BasicButton
-        label="PRZYZNAJ NAGRODĘ"
-      />
-      <BasicButton
-        label="DAJ KONSEKWENCJĘ"
-      />
-
+        <BasicButton
+          label="PRZYZNAJ NAGRODĘ"
+        />
+        <BasicButton
+          label="DAJ KONSEKWENCJĘ"
+        />
+      </Container>
     </>
 
   );
