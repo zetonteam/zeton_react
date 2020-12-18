5b04cface437822a6cd90e58645af8d836a97e06
Paweł <pawel.nieczuja@onet.eu>
2020-12-18 19:48:56 +0100

    Udate color

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,12 +3,13 @@ import { ButtonGroup, Button } from '@material-ui/core';
 import SettingsIcon from '@material-ui/icons/Settings';
 import EmojiEventsIcon from '@material-ui/icons/EmojiEvents';
 import SentimentDissatisfiedIcon from '@material-ui/icons/SentimentDissatisfied';
+import HomeIcon from '@material-ui/icons/Home';
 
 const Footer = () => {
   return (
     <footer className="profiel__footer">
       <ButtonGroup disableElevation variant="contained" color="primary" aria-label="text primary button group">
-        <Button size="large">Home</Button>
+        <Button ><HomeIcon/></Button>
         <Button ><EmojiEventsIcon /></Button>
         <Button ><SentimentDissatisfiedIcon /></Button>
         <Button>Info</Button>
