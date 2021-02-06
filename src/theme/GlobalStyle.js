7be07195b31522938b5d22951b45ad99acb15bf6
Paweł <pawel.nieczuja@onet.eu>
2021-02-06 19:04:17 +0100

    Add GlobalStyle

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap');
+ 
+    *, *::before, *::after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html {
+    font-size: 14px;
+  }
+
+  body {
+    font-size: 1.6rem;
+    font-family: 'Roboto', sans-serif;
+    padding-left: 150px;
+  }
+`;
+
+export default GlobalStyle;
