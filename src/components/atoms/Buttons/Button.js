d7fb9a360690a932326d8fed9affa86337f09507
Paweł <pawel.nieczuja@onet.eu>
2021-02-08 12:36:06 +0100

    Add xs media style for button

--- src/components/atoms/Buttons/Button.js
+++ src/components/atoms/Buttons/Button.js
@@ -15,6 +15,15 @@ const Button = styled.button`
   border-radius: ${({ theme }) => theme.radius};
   letter-spacing: ${({ theme }) => theme.spacing};
   transition: 0.2s;
+
+  @media only screen and (max-width: ${({ theme }) =>
+  theme.mediaMaxSize.xs}) {
+    font-size: ${({ theme }) =>
+    theme.fontSize.xxs};
+    width: 208px;
+    height: 46px;
+    padding: 12px;
+  }
 `;
 
 export default Button;
