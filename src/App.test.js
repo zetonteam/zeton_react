095e6197d4aee2d32137130de279e295483a04d1
Leszek Miotk <tgb.dc7@gmail.com>
2021-02-21 14:13:00 +0100

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/witaj w aplikacji/i);
+  const linkElement = getByText(/wybierz podopiecznego/i);
   expect(linkElement).toBeInTheDocument();
 });
