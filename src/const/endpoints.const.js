9aa4311269c91613dab8c8c747cb4b484c552264
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2022-06-11 16:52:53 +0200

    22-06-refactoring - Refactoring struktury projektu...

---
+++ src/const/endpoints.const.js
@@ -0,0 +1,9 @@
+export const ENDPOINTS = {
+  studentsList: "/api/users/students/",
+  prizesList: "api/users/prizes/",
+};
+
+export const MOCK_ENDPOINTS = {
+  studentsList: "...",
+  prizesList: "...",
+};
